08de6fd6959300b8b4786d0f186b25c02babd4f3
latin bubel <esterapietkiewicz0110@gmail.com>
2024-05-23 19:22:21 +0200

    Append functionality (adds to the list)

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ function updateCity(event) {
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
+  citiesElement.innerHTML += `
   <div class="city">
     <div>
       <h2>${cityName}</h2>
